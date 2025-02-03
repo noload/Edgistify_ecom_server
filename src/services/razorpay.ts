ecc8d333b9d6420d0aecf776ecf42324fc07da88
vaibhav kamble <vaibhavkamble023@gmail.com>
2025-02-03 14:53:28 +0530

    added build and start command

--- src/services/razorpay.ts
+++ src/services/razorpay.ts
@@ -2,8 +2,6 @@ import Razorpay from "razorpay";
 import dotenv from "dotenv"
 dotenv.config();
 
-console.log(process.env.key_id,process.env.key_secret);
-
 const razorpay = new Razorpay({
   key_id: process.env.key_id as string,
   key_secret: process.env.key_secret as string,
--- src/validation.ts/userValidation.ts
+++ src/validation.ts/userValidation.ts
@@ -3,20 +3,20 @@ import Joi from "joi";
 import passwordComplexity from "joi-password-complexity";
 
 const complexityOptions = {
-    min: 6,
-    max: 30,
-    lowerCase: 1,
-    upperCase: 1,
-    numeric: 1,
-    symbol: 1,
-    requirementCount: 2,
-  };
+  min: 6,
+  max: 30,
+  lowerCase: 1,
+  upperCase: 1,
+  numeric: 1,
+  symbol: 1,
+  requirementCount: 2,
+};
 
 export const validateRegisterUser = (
   req: Request,
   res: Response,
   next: NextFunction
-):Promise<any> | void => {
+): Promise<any> | void => {
   try {
     const userRegisterSchema = Joi.object({
       fullName: Joi.string().min(3).required().messages({
@@ -36,48 +36,42 @@ export const validateRegisterUser = (
     const { error } = userRegisterSchema.validate(req.body);
 
     if (error) {
-        console.log(error);
-        res.status(400).json({ message: error.details[0].message });
-    }else{
-        next();
+      console.log(error);
+      res.status(400).json({ message: error.details[0].message });
+    } else {
+      next();
     }
-   
   } catch (error) {
     console.log(`Error in middleware`, error);
-      res.status(500).json({message:"Failed user registration validation"});
+    res.status(500).json({ message: "Failed user registration validation" });
   }
 };
 
-
 export const validateLoginUser = (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ):Promise<any> | void => {
-    try {
-      const userLoginSchema = Joi.object({
-    
-        email: Joi.string().email().required().messages({}),
-  
-        password: passwordComplexity().messages({
-          "any.required": "Password is required",
-          "string.base": "Password must be a string",
-          "string.min": "Password must be at least 6 characters long",
-        }),
-      });
-  
-      const { error } = userLoginSchema.validate(req.body);
-  
-      if (error) {
-          console.log(error);
-          res.status(400).json({ message: error.details[0].message });
-      }else{
-          next();
-      }
-     
-    } catch (error) {
-      console.log(`Error in middleware`, error);
-        res.status(500).json({message:"Failed user login validation"});
-    }
-  };
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> | void => {
+  try {
+    const userLoginSchema = Joi.object({
+      email: Joi.string().email().required().messages({}),
 
+      password: passwordComplexity().messages({
+        "any.required": "Password is required",
+        "string.base": "Password must be a string",
+        "string.min": "Password must be at least 6 characters long",
+      }),
+    });
+
+    const { error } = userLoginSchema.validate(req.body);
+
+    if (error) {
+      res.status(400).json({ message: error.details[0].message });
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(`Error in middleware`, error);
+    res.status(500).json({ message: "Failed user login validation" });
+  }
+};
